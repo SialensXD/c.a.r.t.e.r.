af20312843fa3f2e0be03d9e9dfbdbb67ccbe82e
SialensXD <just.sialens@gmail.com>
2026-09-06 16:03:35 +0300

    ну все

--- ai_handler.py
+++ ai_handler.py
@@ -7,7 +7,7 @@ from groq import AsyncGroq
 from config import GROQ_API_KEY, GROQ_MODEL, OWNER_NAME
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(name)
+logger = logging.getLogger(__name__)
 
 
 class AIHandler:
